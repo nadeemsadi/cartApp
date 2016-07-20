213c6a48afc025c721e4103e9fde1a79826336d0
Nadeem Sadiq <nadeemsadi@hotmail.com>
2016-07-20 00:19:19 -0400

    Initial Commit

---
+++ app/controllers/page_controller.rb
@@ -0,0 +1,16 @@
+class PageController < ApplicationController
+  
+  before_action :authenticate_user!, except: [:home, :about, :faqs]
+  
+  def home
+  end
+
+  def about
+  end
+
+  def faqs
+  end
+
+  def contact
+  end
+end
